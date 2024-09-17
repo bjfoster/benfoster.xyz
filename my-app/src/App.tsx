815822ff954c23db0ba8ca4322b1104ed78b8902
Ben Foster <bjfoster.wrk@gmail.com>
2024-09-17 16:34:25 +0100

    Minor changes.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,22 +2,42 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { AiFillGithub, AiFillLinkedin, AiOutlineMail } from "react-icons/ai";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Welcome to my homepage!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='Link-box'>
+          <a
+            className="App-link"
+            href="mailto:ben@benfoster.xyz"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AiOutlineMail/>
+          </a>
+          <a
+            className="App-link"
+            href="https://www.linkedin.com/in/benjaminfoster1/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AiFillLinkedin/>
+          </a>
+          <a
+            className="App-link"
+            href="https://github.com/bjfoster"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AiFillGithub/>
+          </a>
+        </div>
       </header>
     </div>
   );
